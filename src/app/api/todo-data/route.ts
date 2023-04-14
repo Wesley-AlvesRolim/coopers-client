a922b4741b3f45b07374159a7d9147c2c763de33
Wesley Alves Rolim <walvesrolim@gmail.com>
2023-04-13 21:06:00 -0300

    feat: add todo-data api endpoint

---
+++ src/app/api/todo-data/route.ts
@@ -0,0 +1,33 @@
+import { type NextApiRequest, type NextApiResponse } from 'next';
+
+const todoData = [
+  {
+    id: '1',
+    description: 'this is a new task',
+    isDone: false,
+  },
+  {
+    id: '2',
+    description: 'Develop the To-do list page',
+    isDone: false,
+  },
+  {
+    id: '4',
+    description: 'Create the drag-and-drop function',
+    isDone: false,
+  },
+  {
+    id: '3',
+    description: 'Get FTP credentials',
+    isDone: true,
+  },
+  {
+    id: '5',
+    description: '',
+    isDone: false,
+  },
+];
+
+export function GET(_req: NextApiRequest, res: NextApiResponse) {
+  return new Response(JSON.stringify(todoData));
+}
