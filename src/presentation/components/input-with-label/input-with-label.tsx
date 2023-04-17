1b2b08e1206f94e4a0392ffba0ce38e79e9f1aa4
Wesley Alves Rolim <walvesrolim@gmail.com>
2023-04-17 10:54:40 -0300

    feat: pass ref to Input

--- src/presentation/components/input-with-label/input-with-label.tsx
+++ src/presentation/components/input-with-label/input-with-label.tsx
@@ -1,4 +1,4 @@
-import { type InputHTMLAttributes, useId } from 'react';
+import { type InputHTMLAttributes, useId, forwardRef, type Ref } from 'react';
 import { Input } from '@/presentation/components';
 import './input-with-label.css';
 
@@ -6,19 +6,18 @@ interface InputWithLabelProps extends InputHTMLAttributes<HTMLInputElement> {
   label: string;
 }
 
-const InputWithLabel = ({
-  label,
-  required,
-  ...inputProps
-}: InputWithLabelProps): JSX.Element => {
+const InputWithLabel = (
+  { label, required, ...inputProps }: InputWithLabelProps,
+  ref: Ref<HTMLInputElement>
+): JSX.Element => {
   const id = useId();
 
   return (
     <div className="input-with-label-container">
       <label htmlFor={id}>{required ?? false ? `${label}*` : label}</label>
-      <Input id={id} required={required} {...inputProps} />
+      <Input id={id} required={required} {...inputProps} ref={ref} />
     </div>
   );
 };
 
-export default InputWithLabel;
+export default forwardRef(InputWithLabel);
--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -1,10 +1,17 @@
-import { type InputHTMLAttributes } from 'react';
+import { type LegacyRef, forwardRef } from 'react';
 import './input.css';
 
-interface InputProps extends InputHTMLAttributes<HTMLInputElement> {}
+interface InputProps
+  extends React.DetailedHTMLProps<
+    React.InputHTMLAttributes<HTMLInputElement>,
+    HTMLInputElement
+  > {}
 
-const Input = (props: InputProps): JSX.Element => {
-  return <input type="text" className="input-component" {...props} />;
+const Input = (
+  props: InputProps,
+  ref: LegacyRef<HTMLInputElement>
+): JSX.Element => {
+  return <input ref={ref} type="text" className="input-component" {...props} />;
 };
 
-export default Input;
+export default forwardRef(Input);
