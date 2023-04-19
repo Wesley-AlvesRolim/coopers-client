66d6d03b13e30268a07edda52883d44deb92fda1
Wesley Alves Rolim <walvesrolim@gmail.com>
2023-04-19 15:26:07 -0300

    feat: add create-account usecase

--- src/domain/use-cases/index.ts
+++ src/domain/use-cases/index.ts
@@ -2,5 +2,6 @@ export * from './add-task';
 export * from './delete-task';
 export * from './edit-task';
 export * from './landing-todo-data';
+export * from './create-account';
 export * from './login';
 export * from './read-tasks';
