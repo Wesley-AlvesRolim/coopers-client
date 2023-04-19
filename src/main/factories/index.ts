3cd8fa5f75fe4051cfe32fa34472ecf4e669fa33
Wesley Alves Rolim <walvesrolim@gmail.com>
2023-04-19 15:28:00 -0300

    feat: add makeCreateAccount factory

--- src/main/factories/index.ts
+++ src/main/factories/index.ts
@@ -1,5 +1,6 @@
 export * from './make-add-task';
 export * from './make-fetch-landing-todo-data';
+export * from './make-create-account';
 export * from './make-login';
 export * from './make-read-tasks';
 export * from './make-edit-task';
