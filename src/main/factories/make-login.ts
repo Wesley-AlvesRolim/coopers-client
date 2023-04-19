2c4f8096a879f2243e2bbf7f6bc8933126f6139b
Wesley Alves Rolim <walvesrolim@gmail.com>
2023-04-19 11:25:02 -0300

    refactor(make-login.ts): remove comment disabling eslint rule

--- src/main/factories/make-login.ts
+++ src/main/factories/make-login.ts
@@ -2,7 +2,6 @@ import { FetchAdapter } from '@/data/adapters';
 import { RemoteLogin } from '@/data/use-cases';
 
 export const makeLogin = (): RemoteLogin => {
-  // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
   const url = `${process.env.NEXT_PUBLIC_SERVER_URL}/auth/login/`;
   const fetchAdapter = new FetchAdapter();
   const login = new RemoteLogin(url, fetchAdapter);
