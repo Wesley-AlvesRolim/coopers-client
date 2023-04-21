b5091b0210b91cca23aee7b869a6be0fe7b7e92e
Wesley Alves Rolim <walvesrolim@gmail.com>
2023-04-21 11:14:51 -0300

    feat: add makeDeleteTasksByIsDone factory

--- src/main/factories/index.ts
+++ src/main/factories/index.ts
@@ -5,3 +5,4 @@ export * from './make-login';
 export * from './make-read-tasks';
 export * from './make-edit-task';
 export * from './make-delete-task';
+export * from './make-delete-tasks-by-is-done';
